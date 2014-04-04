4a2478d67d8b007fc0c08342b371df1fdf81c7d2
Louis Orleans <louis@0rleans.com>
2014-04-03 20:27:12 -0700

    Initial move to Github commit

---
+++ bin/Assets/Scripts/ChangeMainPlatformPosition.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeMainPlatformPosition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float _width = ((GameObject.Find ("ImageTarget-PlatformRight").transform.position.x == 0) ? GameObject.Find ("ImageTarget-PlatformLeft").transform.position.x : GameObject.Find ("ImageTarget-PlatformRight").transform.position.x) / 10;
+		//find right platform x set to 0, find left platform in terms of x, width = left.x, position = left.x/2
+
+		transform.localScale = new Vector3(_width * 2, 0.02f, 0.976f);
+		transform.position = new Vector3(_width * 2, transform.position.y, transform.position.z);
+	}
+}
---
+++ bin/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+	public int jumpForce = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		GameObject _Player1 = GameObject.Find ("Player1");
+		if (Input.GetKey ("a")) {
+			print("bang");
+			_Player1.transform.position = new Vector3(_Player1.transform.position.x - 0.1f,_Player1.transform.position.y,_Player1.transform.position.z);
+		}
+		if (Input.GetKey ("d")) {
+			print ("boom");
+			_Player1.transform.position = new Vector3(_Player1.transform.position.x + 0.1f,_Player1.transform.position.y,_Player1.transform.position.z);
+		}
+
+
+	}
+}
