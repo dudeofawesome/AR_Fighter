fc3adebc7cf22961d108bb111f22ae3209b44b91
Louis Orleans <louis@0rleans.com>
2014-04-04 16:32:21 -0700

    player movement now working

--- bin/Assets/Scripts/CharacterController.cs
+++ bin/Assets/Scripts/CharacterController.cs
@@ -3,29 +3,47 @@ using System.Collections;
 
 public class CharacterController : MonoBehaviour {
 
-	public int jumpForce = 10;
+	public int jumpForce = 500000;
+	public int movementForce = 10000;
+	public float drag = 0.3f;
+
+	public string keyUp = "w";
+	public string keyLeft = "a";
+	public string keyDown = "s";
+	public string keyRight = "d";
+
 
 	// Use this for initialization
 	void Start () {
-	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (keyUp)) {
+			rigidbody.velocity = new Vector3(rigidbody.velocity.x,0,0);
+			rigidbody.AddForce(new Vector3(0,jumpForce,0),ForceMode.Force);
+		}
+
+		//apply max velocity
+		if (rigidbody.velocity.x > 20f || rigidbody.velocity.x < -20f) 
+			rigidbody.velocity = new Vector3 (Mathf.Clamp (rigidbody.velocity.x, -20f, 20f), rigidbody.velocity.y, 0);
 	}
 
 	void FixedUpdate () {
-		GameObject _Player1 = GameObject.Find ("Player1");
-		if (Input.GetKey ("a")) {
-			print("bang");
-			_Player1.transform.position = new Vector3(_Player1.transform.position.x - 0.5f,_Player1.transform.position.y,_Player1.transform.position.z);
+		if (Input.GetKey (keyLeft)) {
+//			Player.transform.position = new Vector3(Player1.transform.position.x - 0.5f,Player1.transform.position.y,Player1.transform.position.z);
+			rigidbody.AddForce(new Vector3(movementForce * -1,0,0));
 		}
-		if (Input.GetKey ("d")) {
-			print ("boom");
-			_Player1.transform.position = new Vector3(_Player1.transform.position.x + 0.5f,_Player1.transform.position.y,_Player1.transform.position.z);
+		if (Input.GetKey (keyRight)) {
+//			Player.transform.position = new Vector3(Player.transform.position.x + 0.5f,Player.transform.position.y,Player.transform.position.z);
+			rigidbody.AddForce(new Vector3(movementForce,0,0));
 		}
 
-
+		if (!Input.GetKey (keyLeft) && !Input.GetKey (keyRight) && transform.position.y < 2.5) {
+			rigidbody.velocity = new Vector3 (rigidbody.velocity.x * (1 - drag), rigidbody.velocity.y, 0);
+		}
+		else if (transform.position.y >= 2.5) {
+			rigidbody.velocity = new Vector3 (rigidbody.velocity.x * (0.95f), rigidbody.velocity.y, 0);
+		}
 	}
 }
