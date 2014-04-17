2ca8fa948d942871c0596d5c07a8965dddab47f2
YamaKe <kevin651102@gmail.com>
2014-04-16 20:54:13 -0700

    Touch Scroll Bar
    Basic touch scrolling without the use of the actual scroll bar.
    Next to do: next stage after level selection, How to play, more
    settings(?)

--- bin/Assets/Scripts/MainMenuGUI.cs
+++ bin/Assets/Scripts/MainMenuGUI.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class MainMenuGUI : MonoBehaviour {
 
-	public GUISkin guiSkin, verticalSkin;
+	public GUISkin guiSkin, verticalSkin, scrollSkin;
 
 	public string mainLevel;
 
@@ -16,6 +16,8 @@ public class MainMenuGUI : MonoBehaviour {
 	private float originalHeight = 480; 
 	private Vector3 scale; 
 
+	private Vector2 scrollPosition = Vector2.zero;
+
 
 
 
@@ -25,18 +27,26 @@ public class MainMenuGUI : MonoBehaviour {
 			print ("thief :P");
 		}
 
-		Screen.orientation = ScreenOrientation.LandscapeRight;
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
 
 		//Application.LoadLevel (mainLevel);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		foreach(Touch touch in Input.touches)
+		{
+				bool fInsideList = IsTouchInsideList (touch.position);
+		
+				if (touch.phase == TouchPhase.Moved) {
+						// dragging
+						scrollPosition.y += touch.deltaPosition.y;
+				}
+		}
 	}
-
-
-
+	
+	
+	
 	void OnGUI () {
 		GUI.skin = guiSkin;
 
@@ -53,10 +63,15 @@ public class MainMenuGUI : MonoBehaviour {
 		case MenuState.MAIN:
 			
 			//GUI.Box (new Rect(0, 0, 800, 480), "");
-			
+
+
+
 			GUI.Label (new Rect(50, 50, 300, 300), "TITLE");
 			//PlayerPrefs.SetString ("name",GUI.TextField (new Rect(100, 200, 100, 50), PlayerPrefs.GetString("name")));
+
+
 			
+
 			if(GUI.Button (new Rect(450, 50, 300, 70), "Start")){
 				menuPosition = MenuState.LEVELLOADER;
 
@@ -78,6 +93,7 @@ public class MainMenuGUI : MonoBehaviour {
 				Application.Quit();
 			}
 			
+
 			
 			break;
 		case MenuState.SETTINGS:
@@ -103,8 +119,33 @@ public class MainMenuGUI : MonoBehaviour {
 
 			break;
 		case MenuState.LEVELLOADER:
+			GUI.skin = scrollSkin;
+
 			GUI.Box (new Rect(50, 20, 500, 70), "Select Visual Style");
 			GUI.Box (new Rect(30, 90, 770, 390),"");
+
+
+		
+
+
+
+			
+			scrollPosition = GUI.BeginScrollView (new Rect (430,100,370,330),
+			                                      scrollPosition, new Rect (0, 0, 340, 420));
+			
+			// Make four buttons - one in each corner. The coordinate system is defined
+			// by the last parameter to BeginScrollView.
+			GUI.Button (new Rect (0,0,340,70), "Dojo in the Trees");
+			GUI.Button (new Rect (0,70,340,70), "Setting2");
+			GUI.Button (new Rect (0,140,340,70), "Setting3");
+			if(GUI.Button (new Rect (0,210,340,70), "Setting4")){
+				menuPosition = MenuState.MAIN;
+			}
+			GUI.Button (new Rect (0,280,340,70), "Setting5");
+			GUI.Button (new Rect (0,350,340,70), "Setting6");
+			// End the scroll view that we began above.
+			GUI.EndScrollView ();
+
 			break;
 		case MenuState.HOWTOPLAY:
 			GUI.Box(new Rect(50, 20, 500, 70), "How To Play");
@@ -128,4 +169,14 @@ public class MainMenuGUI : MonoBehaviour {
 	
 	}
 
+	bool IsTouchInsideList(Vector2 touchPos)
+	{
+		Vector2 screenPos    = new Vector2(touchPos.x, Screen.height - touchPos.y);  // invert y coordinate
+		Rect rAdjustedBounds = new Rect(430, 100, 370, 330);
+		
+		return rAdjustedBounds.Contains(screenPos);
+	}
+
 }
+
+
